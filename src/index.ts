5fe758ee8b6409cdf97ff884db94be29ced6da01
Ayo Alfonso <alfonsoayo7@gmail.com>
2025-01-30 06:22:45 +0000

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { MilestoneProgressBar } from "./components/MilestoneProgressBar";
+export type {
+  MilestoneProgressBarProps,
+  IMilestoneStatus,
+} from "./components/MilestoneProgressBar";
